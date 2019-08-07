65f60c9b1c25cd459a3837009f5637ec810dc757
tanya290498 <29tatyana04@gmail.com>
2019-08-07 17:43:57 +0300

    roles of user has been added

---
+++ health/src/app/core/models/role.ts
@@ -0,0 +1,7 @@
+export class Role {
+  public static Admin = 'Admin';
+  public static Clinic = 'Clinic';
+  public static Patient = 'Patient';
+  public static Investor = 'Investor';
+  public static ClinicAdmin = 'ClinicAdmin';
+}
